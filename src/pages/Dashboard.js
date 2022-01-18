01f40aa7b5caaeba2341ddc6ccb90c0b9dc45c65
unknown <nids.kashyap2021@gmail.com>
2022-01-18 14:22:07 +0530

    Styled the dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,17 @@
-//import "../assets/Dashboard.css"
+import "../assets/Dashboard.css"
 const Dashboard = () => {
-    return(
+    return (
         <>
-            <h1>Hello!</h1>
+            {/* <!-- hero section-- > */}
+            <header className="hero-section">
+                <div className="content">
+                    {/* <img src="/logo.jpeg" className="logo" alt=""></img> */}
+                    <p className="heading">Eat.Think.Safe</p>
+                    {/* <br> */}
+                    <p className="sub-heading">Eat Everything You Buy</p>
+                </div>
+            </header>
+            {/* <h1>Hello!</h1> */}
             <div>
                 <h3>Items in your Fridge</h3>
                 <ul>
@@ -14,9 +23,9 @@ const Dashboard = () => {
             <div>
                 <h3>Add Food Info</h3>
                 <label htmlFor="foodItem">Add Food Item</label>
-                <input type="text" placeholder="Food Item" name="foodItem"/>
+                <input type="text" placeholder="Food Item" name="foodItem" />
                 <label>Add its Expiry Date</label>
-                <input type="date" name="expiryDate"/>
+                <input type="date" name="expiryDate" />
                 <button className="btn btn-danger">Add Item</button>
             </div>
             <div>
