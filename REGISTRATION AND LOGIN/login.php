3d884e68ab027ff0e28e1fc4602566e48e59779d
Manasi <manasighutukade@gmail.com>
2022-01-18 21:47:59 +0530

    minor

--- REGISTRATION AND LOGIN/login.php
+++ REGISTRATION AND LOGIN/login.php
@@ -37,25 +37,25 @@ if(empty($err))
     if(mysqli_stmt_execute($stmt)){
         mysqli_stmt_store_result($stmt);
         if(mysqli_stmt_num_rows($stmt) == 1)
-        {
-            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-            if(mysqli_stmt_fetch($stmt))
-            {
-                if(password_verify($password, $hashed_password))
                 {
-                    // this means the password is corrct. Allow user to login
-                    session_start();
-                    $_SESSION["username"] = $username;
-                    $_SESSION["id"] = $id;
-                    $_SESSION["loggedin"] = true;
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt))
+                    {
+                        if(password_verify($password, $hashed_password))
+                        {
+                            // this means the password is corrct. Allow user to login
+                            session_start();
+                            $_SESSION["username"] = $username;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["loggedin"] = true;
 
-                    //Redirect user to welcome page
-                    header("location: welcome.php");
-                    
+                            //Redirect user to welcome page
+                            header("location: welcome.php");
+                            
+                        }
+                    }
+
                 }
-            }
-
-        }
 
     }
 }    
@@ -66,93 +66,73 @@ if(empty($err))
 
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="registernew.css">
-</head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<body>
-    <div class="panel">
+    <title>PHP login system!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Php Login System</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+  <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="registernew.php">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
 
-        <!--<div class="pricing-plan">
-            <img src="icons/icon1.png" alt="" class="pricing-img">
-            <h2 class="pricing-header">Personal</h2>
-            <ul class="pricing-features">
-                <li class="pricing-features-item">Custom domains</li>
-                <li class="pricing-features-item">Sleeps after 30 mins of inactivity</li>
-            </ul>
-            <span class="pricing-price">Free</span>
-            <a href="#/" class="pricing-button">Sign up</a>
-        </div>-->
-        <form action="" method="post">
-            <div class="pricing-plan">
-                <img src="icons/icon2.png" alt="" class="pricing-img">
-                <h2 class="pricing-header">Website name</h2>
-                <ul class="pricing-features">
-                    <li class="pricing-features-item">Enter your Email(username) : </li>
-                    <input type="text" class="form-control" name="username" id="inputEmail4" placeholder="Email">
-                    <li class="pricing-features-item">Enter your password : </li>
-                    <input type="password" class="form-control" name="password" id="inputEmail4"
-                        placeholder="Password">
-                   
-                </ul>
-            </div>
+      
+     
+    </ul>
+  </div>
+</nav>
 
-            
-        
-       
-            <!-- <div class="form-row">
-                <div class="form-group username">
-                    <label for="inputEmail4">Choose your Username</label>
-                    <input type="text" class="form-control" name="username" id="inputEmail4" placeholder="Email">
-                </div>
-                <div class="form-group password">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control" name="password" id="inputPassword4"
-                        placeholder="Password">
-                </div>
-            </div>
-            <div class="form-group confirmation">
-                <label for="inputPassword4">Confirm Password</label>
-                <input type="password" class="form-control" name="confirm_password" id="inputPassword"
-                    placeholder="Confirm Password">
-            </div>
+<div class="container mt-4">
+<h3>Please Login Here:</h3>
+<hr>
+
+<form action="" method="post">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Username</label>
+    <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary" onclick="welcome.php">Submit</button>
+</form>
 
 
-            <div class="form-group">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                    <label class="form-check-label" for="gridCheck">
-                        Check me out
-                    </label>
-                </div>
-            </div> -->
-           <button type="submit" id="button">Login</button>
-        </form>
-    </div> 
-       
-   
-    <!-- <div class="pricing-plan">
-            <img src="icons/icon3.png" alt="" class="pricing-img">
-            <h2 class="pricing-header">Enterprise</h2>
-            <ul class="pricing-features">
-                <li class="pricing-features-item">Dedicated</li>
-                <li class="pricing-features-item">Simple horizontal scalability</li>
-            </ul>
-            <span class="pricing-price">$400</span>
-            <a href="#/" class="pricing-button">Free trial</a>
-        </div> -->
 
-    </div>
+</div>
 
-</body>
-
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+$insert = false;
+if(isset($_POST['desc'])){
+    // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+
+    // Collect post variables
+   
+    $desc = $_POST['desc'];
+    $name = $_POST['user'];
+    $sql = "INSERT INTO `login`.`fridge` (`name`,`data`, `date`) VALUES ('$name','$desc', current_timestamp);";
+    // echo $sql;
+
+    // Execute the query
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to Travel Form</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="normie.css">
+</head>
+
+<body>
+
+  <div class="container">
+    <h1>Enter your items as shown in the below example</h1>
+    <img src="image.jpg" class="img">
+
+    <?php
+        if($insert == true){
+        echo "<p class='submitMsg'>Your item was added</p>";
+        }
+    ?>
+    <form action="index.php" method="post">
+      Enter your username
+      <input type="email" name="user" placeholder="Email"><br>
+      Enter the items in your fridge along with the expiry date <br>
+      <textarea name="desc" id="desc" cols="30" rows="10" placeholder="'item name' and 'dd/mm/yyyy"></textarea><br>
+      <button class="btn">Submit</button>
+    </form>
+  </div>
+
+
+</body>
+
+</html>
